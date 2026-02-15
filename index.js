8d3e096eb90cfea514a569a26749ae164281c8d3
fd-Martin <fdmosharof@gmail.com>
2026-02-15 18:22:05 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+const admin = require("firebase-admin");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+
+
+
+
+
+const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.pstqy5z.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
