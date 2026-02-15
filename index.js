4a59da73acd7f0481251415470c47780eddb902a
fd-Martin <fdmosharof@gmail.com>
2026-02-15 20:01:23 +0600

    revision

--- index.js
+++ index.js
@@ -564,14 +564,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
 app.get("/", async (req, res) => {
   res.send("book courier is working");
 });
